ceae26b4b4a69625bbd4e9b165343d60794c1259
addission <108756447+aad999@users.noreply.github.com>
2023-03-24 19:43:20 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
       'md': '768px',
       // => @media (min-width: 768px) { ... }
 
-      'lg': '1200px',
+      'lg': '1024px',
       // => @media (min-width: 1024px) { ... }
 
       'xl': '1280px',
